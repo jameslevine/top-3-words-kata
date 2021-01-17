fe5451c4593100b78bc05df19b28d7d30b22f3c2
James <jamesalexanderlevine@gmail.com>
2021-01-17 22:22:43 +0000

    add function test for findTopWords

--- src/test/utils/utils.spec.js
+++ src/test/utils/utils.spec.js
@@ -15,7 +15,7 @@ describe("formatString function test", () => {
 });
 
 describe("calculateWordFrequencies function test", () => {
-  test("check correct frequencies", () => {
+  test("check correct frequencies are returned and sorted correctly by count", () => {
     const sentence =
       "in a village of la mancha the name of which i have no desire to call to mind there lived not long since one of those gentlemen that keep a lance in the lancerack an old buckler a lean hack and a greyhound for coursing an olla of rather more beef than mutton a salad on most nights scraps on saturdays lentils on fridays and a pigeon or so extra on sundays made away with threequarters of his income";
     const expected = [
@@ -83,5 +83,77 @@ describe("calculateWordFrequencies function test", () => {
       { name: "income", count: 1 },
     ];
     expect(calculateWordFrequencies(sentence)).toEqual(expected);
+  });
+});
+
+describe("findTopWords function test", () => {
+  test("top 3 words are returned correctly", () => {
+    const wordCounts = [
+      { name: "a", count: 6 },
+      { name: "of", count: 5 },
+      { name: "on", count: 4 },
+      { name: "in", count: 2 },
+      { name: "the", count: 2 },
+      { name: "to", count: 2 },
+      { name: "an", count: 2 },
+      { name: "and", count: 2 },
+      { name: "village", count: 1 },
+      { name: "la", count: 1 },
+      { name: "mancha", count: 1 },
+      { name: "name", count: 1 },
+      { name: "which", count: 1 },
+      { name: "i", count: 1 },
+      { name: "have", count: 1 },
+      { name: "no", count: 1 },
+      { name: "desire", count: 1 },
+      { name: "call", count: 1 },
+      { name: "mind", count: 1 },
+      { name: "there", count: 1 },
+      { name: "lived", count: 1 },
+      { name: "not", count: 1 },
+      { name: "long", count: 1 },
+      { name: "since", count: 1 },
+      { name: "one", count: 1 },
+      { name: "those", count: 1 },
+      { name: "gentlemen", count: 1 },
+      { name: "that", count: 1 },
+      { name: "keep", count: 1 },
+      { name: "lance", count: 1 },
+      { name: "lancerack", count: 1 },
+      { name: "old", count: 1 },
+      { name: "buckler", count: 1 },
+      { name: "lean", count: 1 },
+      { name: "hack", count: 1 },
+      { name: "greyhound", count: 1 },
+      { name: "for", count: 1 },
+      { name: "coursing", count: 1 },
+      { name: "olla", count: 1 },
+      { name: "rather", count: 1 },
+      { name: "more", count: 1 },
+      { name: "beef", count: 1 },
+      { name: "than", count: 1 },
+      { name: "mutton", count: 1 },
+      { name: "salad", count: 1 },
+      { name: "most", count: 1 },
+      { name: "nights", count: 1 },
+      { name: "scraps", count: 1 },
+      { name: "saturdays", count: 1 },
+      { name: "lentils", count: 1 },
+      { name: "fridays", count: 1 },
+      { name: "pigeon", count: 1 },
+      { name: "or", count: 1 },
+      { name: "so", count: 1 },
+      { name: "extra", count: 1 },
+      { name: "sundays", count: 1 },
+      { name: "made", count: 1 },
+      { name: "away", count: 1 },
+      { name: "with", count: 1 },
+      { name: "threequarters", count: 1 },
+      { name: "his", count: 1 },
+      { name: "income", count: 1 },
+    ];
+    const numberOfWords = 3;
+    const expected = ["a", "of", "on"];
+    expect(findTopWords(wordCounts, numberOfWords)).toStrictEqual(expected);
   });
 });
